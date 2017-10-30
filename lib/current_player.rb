acb83cf1bcf864719c47d0bfb6f586a1b615c77f
Eddie Fisher <eddief28@tiscali.co.uk>
2017-10-30 05:33:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@
 def turn_count(board)
   counter = 0
   board.each do |value|
-    if value == "X" || value == "O"
-counter += 1
-end
+    ( value == "X" || value == "O"
+counter += 1 :
+)
 end
 return counter
 end
